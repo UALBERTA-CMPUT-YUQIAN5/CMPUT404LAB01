5b38a3fd618a14aab4095033a6afa496b9246808
yuqian5 <yuqian5@ualberta.ca>
2022-01-11 01:47:20 -0500

    sync

--- request_version.py
+++ request_version.py
@@ -2,6 +2,8 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get('http://google.com/')
+#r = requests.get('http://google.com/')
+r= requests.get("https://raw.githubusercontent.com/yuqian5/CMPUT404LAB01/master/request_version.py")
+
 print(r)
 print(r.text)
